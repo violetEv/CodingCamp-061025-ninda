dd73f4295204287fc605408a07585fbfb7ab6811
violetEv <ninda2752@gmail.com>
2025-10-11 09:44:28 +0700

    Add burger menu functionality and style adjustments for navigation

--- js/script.js
+++ js/script.js
@@ -27,3 +27,15 @@ function validateForm() {
         alert("Thank you for your message, " + name + "!");
     }
 }
+
+// burger
+  const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const navLists = document.querySelector("nav");
+
+    burger.addEventListener("click", () => {
+      navLists.classList.toggle("nav-active");
+    });
+  };
+
+  navSlide();
